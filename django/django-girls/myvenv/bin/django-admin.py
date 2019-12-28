a484551ee6684c59358570951d8a73fcad666611
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2019-12-29 02:18:53 +0530

    django-setup

---
+++ django/django-girls/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/beingsaurabh/Documents/web-dev/django/django-girls/myvenv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
