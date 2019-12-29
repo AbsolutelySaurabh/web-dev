4b2a6fbe6d2626aec1720f3ffb26864db83f16cb
AbsolutelySaurabh <absolutelysaurabh03@gmail.com>
2019-12-29 12:50:09 +0530

    django models

--- django/django-girls/mysite/settings.py
+++ django/django-girls/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@e5#n28t-q!*-#6200+t!v_%m(817)msi969^(ldb&#2jgnn=_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
